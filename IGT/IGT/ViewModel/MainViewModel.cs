3dd598b54da2076aeea71b4b77970dae63fd6986
AD <AD@Desktop>
2016-12-07 21:50:32 +0100

    Initial

---
+++ IGT/IGT/ViewModel/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using GalaSoft.MvvmLight;
+using IGT.Model;
+
+namespace IGT.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// See http://www.mvvmlight.net
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel(IDataService dataService)
+        {
+        }
+    }
+}
